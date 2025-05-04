8db5345c8d1163364c30e896c56134117a242762
ShunheWang <maisonwangsh@gmail.com>
2025-05-04 16:38:41 +0800

    feature: 对FreeMarker动态模版生成发放封装;

--- maison-generator-basic/src/main/java/com/maison/generator/DynamicGenerator.java
+++ maison-generator-basic/src/main/java/com/maison/generator/DynamicGenerator.java
@@ -1,5 +1,6 @@
 package com.maison.generator;
 
+import cn.hutool.core.io.FileUtil;
 import com.maison.model.MainTemplateConfig;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
@@ -13,37 +14,52 @@ import java.io.Writer;
 public class DynamicGenerator {
 
     public static void main(String[] args) throws IOException, TemplateException {
+        String projectPath = System.getProperty("user.dir") + File.separator + "maison-generator-basic";
+        String inputPath = projectPath + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        String outputPath = projectPath + File.separator + "MainTemplate.java";
+        MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
+        mainTemplateConfig.setAuthor("maison");
+        mainTemplateConfig.setOutputText("求和结果：");
+        mainTemplateConfig.setLoop(true);
+        doGenerate(inputPath, outputPath, mainTemplateConfig);
+    }
 
-
-
+    /**
+     * 生成文件
+     *
+     * @param inputPath  模板文件输入路径
+     * @param outputPath 输出路径
+     * @param model      数据模型
+     * @throws IOException
+     * @throws TemplateException
+     */
+    public static void doGenerate(String inputPath, String outputPath, Object model) throws IOException, TemplateException {
         // new 出 Configuration 对象，参数为 FreeMarker 版本号
         Configuration configuration = new Configuration(Configuration.VERSION_2_3_23);
 
-        String projectPath = System.getProperty("user.dir") + File.separator + "maison-generator-basic";
-        File parentFile = new File(projectPath);
-        File file = new File(parentFile, "src/main/resources/templates");
-
         // 指定模板文件所在的路径
-        configuration.setDirectoryForTemplateLoading(file);
+        File templateDir = new File(inputPath).getParentFile();
+        configuration.setDirectoryForTemplateLoading(templateDir);
 
         // 设置模板文件使用的字符集
         configuration.setDefaultEncoding("utf-8");
 
         // 创建模板对象，加载指定模板
-        Template template = configuration.getTemplate("MainTemplate.java.ftl");
+        String templateName = new File(inputPath).getName();
+        Template template = configuration.getTemplate(templateName);
 
-        // 数据模型
+        // 创建数据模型
         MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
         mainTemplateConfig.setAuthor("maison");
-        mainTemplateConfig.setOutputText("输出结果");
+        mainTemplateConfig.setOutputText("求和结果：");
         mainTemplateConfig.setLoop(false);
 
-
-        Writer out = new FileWriter("MainTemplate.java");
-
-        template.process(mainTemplateConfig, out);
+        // 生成
+        Writer out = new FileWriter(outputPath);
+        template.process(model, out);
 
         // 生成文件后别忘了关闭哦
         out.close();
     }
 }
+
