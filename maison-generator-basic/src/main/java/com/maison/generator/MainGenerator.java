63288b6b7aae4f971e460b8bfa2716d9e6017f7c
ShunheWang <maisonwangsh@gmail.com>
2025-05-11 17:25:52 +0800

    feature: 静态动态生成文件测试

---
+++ maison-generator-basic/src/main/java/com/maison/generator/MainGenerator.java
@@ -0,0 +1,61 @@
+package com.maison.generator;
+
+import com.maison.model.MainTemplateConfig;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainGenerator {
+
+//    public static void main(String[] args) throws TemplateException, IOException {
+//        // 1. 静态文件生成
+//        String projectPath = System.getProperty("user.dir");
+//        String inputPath = projectPath + File.separator + "maison-generator-demo-projects" + File.separator + "acm-template";
+//        String outputPath = projectPath;
+//        StaticGenerator.copyFilesByRecursive(inputPath, outputPath);
+//
+//        // 2. 动态文件生成
+////        String projectPath = System.getProperty("user.dir") + File.separator + "maison-generator-basic";
+//        String dynamicInputPath = projectPath + File.separator + "maison-generator-basic" + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+//        String dynamicOutputPath = projectPath + File.separator + "acm-template/src/com/yupi/acm/MainTemplate.java";
+//        MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
+//        mainTemplateConfig.setAuthor("maison");
+//        mainTemplateConfig.setOutputText("求和输出：");
+//        mainTemplateConfig.setLoop(true);
+//        DynamicGenerator.doGenerate(dynamicInputPath, dynamicOutputPath, mainTemplateConfig);
+//    }
+
+    /**
+     * 生成
+     *
+     * @param model 数据模型
+     * @throws TemplateException
+     * @throws IOException
+     */
+    public static void doGenerate(Object model) throws TemplateException, IOException {
+        String projectPath = System.getProperty("user.dir");
+        // 整个项目的根路径
+        File parentFile = new File(projectPath).getParentFile();
+        // 输入路径
+        String inputPath = new File(parentFile, "maison-generator-demo-projects/acm-template").getAbsolutePath();
+        String outputPath = projectPath;
+        // 生成静态文件
+        StaticGenerator.copyFilesByRecursive(inputPath, outputPath);
+        // 生成动态文件
+        String inputDynamicFilePath = projectPath + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        String outputDynamicFilePath = outputPath + File.separator + "acm-template/src/com/maison/acm/MainTemplate.java";
+        DynamicGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
+    }
+
+    public static void main(String[] args) throws TemplateException, IOException {
+        MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
+        mainTemplateConfig.setAuthor("maison");
+        mainTemplateConfig.setLoop(false);
+        mainTemplateConfig.setOutputText("求和结果：");
+        doGenerate(mainTemplateConfig);
+    }
+}
+
+
+
--- maison-generator-basic/src/main/resources/templates/MainTemplate.java.ftl
+++ maison-generator-basic/src/main/resources/templates/MainTemplate.java.ftl
@@ -1,4 +1,4 @@
-package com.yupi.acm;
+package com.maison.acm;
 
 import java.util.Scanner;
 
