c3e2004f800345098493982d52f46a6c8278be5f
ShunheWang <maisonwangsh@gmail.com>
2025-05-25 23:46:07 +0800

    feature: init 生成器工具项目;

---
+++ maison-generator-maker/src/main/java/com/maison/maker/Main.java
@@ -0,0 +1,16 @@
+package com.maison.maker;
+
+import com.maison.maker.cli.CommandExecutor;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        args = new String[]{"generate", "-l", "-a", "-o"};
+//        args = new String[]{"config"};
+//        args = new String[]{"list"};
+        CommandExecutor commandExecutor = new CommandExecutor();
+        commandExecutor.doExecute(args);
+
+    }
+}
---
+++ maison-generator-maker/src/main/java/com/maison/maker/model/DataModel.java
@@ -0,0 +1,13 @@
+package com.maison.maker.model;
+
+import lombok.Data;
+
+@Data
+public class DataModel {
+
+    private String author;
+
+    private String outputText;
+
+    private boolean loop;
+}
