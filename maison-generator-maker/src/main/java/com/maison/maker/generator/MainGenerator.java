23a3675c6537d7432c67b8603b5fba6936251fad
ShunheWang <maisonwangsh@gmail.com>
2025-06-01 13:50:14 +0800

    feature: 增加generator文件夹中及调用方法测试;

--- maison-generator-maker/src/main/java/com/maison/maker/generator/MainGenerator.java
+++ maison-generator-maker/src/main/java/com/maison/maker/generator/MainGenerator.java
@@ -66,6 +66,21 @@ public class MainGenerator {
         outputFilePath = outputBaseJavaPackagePath + "/Main.java";
         DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
 
+        inputFilePath = inputResourcePath + File.separator + "templates/java/generator/DynamicGenerator.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/generator/DynamicGenerator.java";
+        DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
+
+        // generator.MainGenerator
+        inputFilePath = inputResourcePath + File.separator + "templates/java/generator/MainGenerator.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/generator/MainGenerator.java";
+        DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
+
+        // generator.StaticGenerator
+        inputFilePath = inputResourcePath + File.separator + "templates/java/generator/StaticGenerator.java.ftl";
+        outputFilePath = outputBaseJavaPackagePath + "/generator/StaticGenerator.java";
+        DynamicFileGenerator.doGenerate(inputFilePath , outputFilePath, meta);
+
+
 
     }
 }
