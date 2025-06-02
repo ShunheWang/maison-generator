5ebb8a6856a4e55798fe35ab6dd7dbb1ab7f7e96
ShunheWang <maisonwangsh@gmail.com>
2025-06-02 15:30:37 +0800

    feature: 模版方法增强代码可扩展性;

--- maison-generator-maker/src/main/java/com/maison/maker/Main.java
+++ maison-generator-maker/src/main/java/com/maison/maker/Main.java
@@ -2,15 +2,18 @@ package com.maison.maker;
 
 //import com.maison.maker.cli.CommandExecutor;
 
+import com.maison.maker.generator.main.MainGenerator;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-//        args = new String[]{"generate", "-l", "-a", "-o"};
-//        args = new String[]{"config"};
-//        args = new String[]{"list"};
-//        CommandExecutor commandExecutor = new CommandExecutor();
-//        commandExecutor.doExecute(args);
+    public static void main(String[] args) throws TemplateException, IOException, InterruptedException {
+        MainGenerator mainGenerator = new MainGenerator();
+        mainGenerator.doGenerate();
+
 
     }
 }
---
+++ maison-generator-maker/src/main/java/com/maison/maker/generator/main/MainGenerator.java
@@ -0,0 +1,11 @@
+package com.maison.maker.generator.main;
+
+
+public class MainGenerator extends GenerateTemplate {
+
+    @Override
+    protected void buildDist(String outputPath, String sourceCopyDestPath, String jarPath, String shellOutputFilePath) {
+        System.out.println("不要给我输出 dist 啦！");
+    }
+
+}
