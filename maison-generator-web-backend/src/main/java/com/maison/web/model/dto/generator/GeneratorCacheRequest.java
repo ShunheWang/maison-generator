85e52cf814dd9a2eff3eb1da5050a4c012c28f76
ShunheWang <maisonwangsh@gmail.com>
2025-08-10 17:18:33 +0800

    feature: 优化下载接口 ;

---
+++ maison-generator-web-backend/src/main/java/com/maison/web/model/dto/generator/GeneratorCacheRequest.java
@@ -0,0 +1,20 @@
+package com.maison.web.model.dto.generator;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * 使用代码生成器请求
+ */
+@Data
+public class GeneratorCacheRequest implements Serializable {
+
+    /**
+     * 生成器的 id
+     */
+    private Long id;
+
+    private static final long serialVersionUID = 1L;
+}
